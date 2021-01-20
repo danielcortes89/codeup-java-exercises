3223099fd4510638522cd74777b1750e12e24506
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-20 12:09:22 -0600

    feat: Finished syntax types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.print("Hello World!");
+
+        int myFavoriteNumber = 123;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "3.14159";
+        System.out.println(myString);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(y);
     }
 }
