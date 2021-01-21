f2419f2b661b16cbf3245be72ca89c42638ac271
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-21 16:17:39 -0600

    feat: One million doesn't work

--- src/syntax/ControlFlowExercise.java
+++ src/syntax/ControlFlowExercise.java
@@ -2,11 +2,22 @@ package syntax;
 
 public class ControlFlowExercise {
     public static void main(String[] args) {
-        int i = 5;
+        int i = 2;
+        int max = 10000;
 
-        while(i <= 15){
+//        do{
+//            System.out.println(i);
+//            i += 2;
+//        } while(i < 100);
+//
+//        do{
+//            System.out.println(i);
+//            i -= 5;
+//        } while(i >= -10);
+
+        do{
             System.out.println(i);
-            i++;
-        }
+            i = i * i;
+        } while(i <= max);
     }
 }
