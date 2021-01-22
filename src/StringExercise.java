9541d13480e45c4359b23c8d7c34391bf8579a9f
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-22 12:09:28 -0600

    feat: String exercise one

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String edu = "We don't need no education";
+        String mind = "We don't need no mind control";
+        String breakString = "Check \" this\" out!, \"s inside of \"s!";
+        String slash = "In windows, the main drive is usually C:\\\n";
+        String amazing = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(edu);
+        System.out.println(mind);
+        System.out.println(breakString);
+        System.out.println(slash);
+        System.out.println(amazing);
+    }
+}
