44a1bcee4c9d3cf22b7c05bed77ceaf80fe5c79d
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-25 09:50:36 -0600

    feat: Finished part 1 exercise

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,21 +1,29 @@
 public class MethodsExercise {
     public static void main(String[] args) {
-
+        addition(2, 2);
+        subtraction(2, 2);
+        multiplication(2, 2);
+        division(2, 2);
+        modulus(2, 2);
     }
 
     public static void addition(int a, int b){
-
+        System.out.println(a + b);
     }
 
     public static void subtraction(int a, int b){
-
+        System.out.println(a - b);
     }
 
     public static void multiplication(int a, int b){
-
+        System.out.println(a * b);
     }
 
     public static void division(int a, int b){
+        System.out.println(a / b);
+    }
 
+    public static void modulus(int a, int b){
+        System.out.println(a % b);
     }
 }
