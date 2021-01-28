d48756e276baf1193e7a2f8bbc314357fcd27680
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-01-28 16:18:39 -0600

    feat: Person constructor initial set up

--- src/syntax/scratch.java
+++ src/syntax/scratch.java
@@ -3,41 +3,45 @@ import java.util.Scanner;
 
 public class scratch {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Hero ourHero = new Hero();
+        int x_coor = 1;
+        int y_coor = 1;
+
         System.out.println("Your health is " + ourHero.health);
         System.out.println("You parachuted on a cliff, where do you go?");
-        String start = "1";
-        String position = start;
-        System.out.println(start);
+        String userDirection = scanner.nextLine();
 
-        position += start;
-        System.out.println(position);
-
-//        position = position + "." + start;
-        position = stepRight(position, start);
-        System.out.println(position);
-
-        int ender = Integer.parseInt(start);
-        ender++;
-
-        String cleanedEnder = String.valueOf(ender);
-
-
-        position = position.replace(start, cleanedEnder);
-        System.out.println(position);
-    }
-
-    public static String stepRight(String position, String start){
-
-        if(position.indexOf(".", 0) > -1){
-            position = position + start;
-
-            return position;
-        } else {
-            position = position + "." + start;
-            return position;
+        if(userDirection.equals("up")){
+            y_coor = y_coor++;
+            System.out.println(y_coor);
+            System.out.println("You stepped on a mine.");
+            ourHero.health = 90;
+            System.out.println("Health is: " + ourHero.health);
+        } else if(userDirection.equals("left")){
+            stepLeft(x_coor);
         }
 
-//        System.out.println("forwards or backwards?");
+
+//        STEP LEFT
+        x_coor = stepLeft(x_coor);
+//        System.out.println(x_coor);
+
+//        STEP RIGHT
+        x_coor = stepRight(x_coor);
+//        System.out.println(x_coor);
+    }
+
+
+
+    public static int stepRight(int position){
+
+//        position++;
+    return ++position;
+
+    }
+
+    public static int stepLeft(int position){
+        return --position;
     }
 }
