4ccc6f558495c47298b2d3a29b9fd321da708346
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-02-01 11:17:41 -0600

    feat: Finished name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+
+public class ServerNameGenerator {
+//    String[] adjectives = {
+//            "happy",
+//            "beautiful",
+//            "adorable",
+//            "small",
+//            "gregarious",
+//            "Gorgeous",
+//            "meaningless",
+//            "nice",
+//            "harsh",
+//            "Super"
+//
+//    };
+//    String[] nouns = {
+//            "Dog",
+//            "cat",
+//            "House",
+//            "Gym",
+//            "Doug",
+//            "Cindy",
+//            "Bike",
+//            "Car",
+//            "Tiger",
+//            "Wolf"
+//    };
+    public static String randomElement(String[] array){
+        Random rn = new Random();
+        int randomIndex = rn.nextInt(9);
+        return array[randomIndex];
+    }
+
+    public static void main(String[] args) {
+        String[] adjectives = {
+                "happy",
+                "beautiful",
+                "adorable",
+                "small",
+                "gregarious",
+                "Gorgeous",
+                "meaningless",
+                "nice",
+                "harsh",
+                "Super"
+
+        };
+        String[] nouns = {
+                "Dog",
+                "cat",
+                "House",
+                "Gym",
+                "Doug",
+                "Cindy",
+                "Bike",
+                "Car",
+                "Tiger",
+                "Wolf"
+        };
+        String noun = randomElement(nouns);
+        String adjective = randomElement(adjectives);
+        System.out.println("Here is your Server name: ");
+        System.out.println(noun + "-" + adjective);
+    }
+}
