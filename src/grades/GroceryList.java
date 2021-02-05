c61126c5dd5469d0cc70929bb388a91623689529
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-02-05 14:35:08 -0600

    feat: Took input logic into a method

--- src/grades/GroceryList.java
+++ src/grades/GroceryList.java
@@ -4,6 +4,7 @@ import util.Input;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class GroceryList {
     public static void main(String[] args) {
@@ -25,60 +26,59 @@ public class GroceryList {
 
             System.out.println("Want to add an item?");
             if(input.getString().equalsIgnoreCase("yes")){
-                System.out.println("Pick a category\n");
 
-                System.out.println("fruit, veg or animal");
-                String ans = input.getString();
 
-                if(ans.equalsIgnoreCase("fruit")){
-                    String nameToAdd;
-                    double quantityToAdd;
-
-                    System.out.println("Enter the item");
-                    nameToAdd = input.getString();
-
-                    System.out.println("And the amount?");
-                    quantityToAdd = input.getInt();
-
-                    HashMap target = groceryList.get("fruit");
-
-                    target.put(nameToAdd, quantityToAdd);
-
-                    System.out.println(target);
-                } else if(ans.equalsIgnoreCase("veg")){
-                    String nameToAdd;
-                    double quantityToAdd;
-
-                    System.out.println("Enter the item");
-                    nameToAdd = input.getString();
-
-                    System.out.println("And the amount?");
-                    quantityToAdd = input.getInt();
-
-                    HashMap target = groceryList.get("veg");
-
-                    target.put(nameToAdd, quantityToAdd);
-
-                    System.out.println(target);
-                } else if(ans.equalsIgnoreCase("animal")){
-                    String nameToAdd;
-                    double quantityToAdd;
-
-                    System.out.println("Enter the item");
-                    nameToAdd = input.getString();
-
-                    System.out.println("And the amount?");
-                    quantityToAdd = input.getInt();
-
-                    HashMap target = groceryList.get("fruit");
-
-                    target.put(nameToAdd, quantityToAdd);
-
-                    System.out.println(target);
-                }
+//                if(ans.equalsIgnoreCase("fruit") || ans.equalsIgnoreCase("veg") || ans.equalsIgnoreCase("animal")){
+//                    String nameToAdd;
+//                    double quantityToAdd;
+//
+//                    System.out.println("Enter the item");
+//                    nameToAdd = input.getString();
+//
+//                    System.out.println("And the amount?");
+//                    quantityToAdd = input.getInt();
+//
+//                    HashMap target = groceryList.get(ans.toLowerCase());
+//
+//                    target.put(nameToAdd, quantityToAdd);
+//
+//                    System.out.println(target);
+//                } else {
+//                    System.out.println("Enter an actual category.");
+//                }
+                addItem(groceryList);
             }
         } else {
             System.out.println("Goodbye");
+        }
+    }
+
+    public static void addItem(HashMap groceryList){
+        Input input = new Input();
+
+        System.out.println("Pick a category\n");
+
+        System.out.println("fruit, veg or animal");
+        String ans = input.getString();
+
+        if(ans.equalsIgnoreCase("fruit") || ans.equalsIgnoreCase("veg") || ans.equalsIgnoreCase("animal")){
+            String nameToAdd;
+            double quantityToAdd;
+
+            System.out.println("Enter the item");
+            nameToAdd = input.getString();
+
+            System.out.println("And the amount?");
+            quantityToAdd = input.getInt();
+
+            HashMap target = (HashMap) groceryList.get(ans.toLowerCase());
+
+            target.put(nameToAdd, quantityToAdd);
+
+            System.out.println(target);
+        } else {
+            System.out.println("Enter an actual category.");
+            addItem(groceryList);
         }
     }
 }
